5c8ba2c86297adf66c9a64bd6c66965b143908dd
Kelvin Santos <kelvinsteinersantos@gmail.com>
2021-10-01 18:10:41 -0300

    Update system_setup.sh

--- system_setup.sh
+++ system_setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-pacman -Sy \
+pacman -Sy
+
+pacman -S \
   sudo man vi vim \
   openssh git rsync curl wget \
   networkmanager avahi nss-mdns \
@@ -8,6 +10,11 @@ pacman -Sy \
   gnome-keyring libsecret \
   brightnessctl acpi \
   arch-install-scripts
+
+pacman -S \
+  sddm \
+  arandr autorandr maim \
+  i3-gaps i3blocks dunst 
 
 systemclt enable NetworkManager
 systemclt start  NetworkManager
