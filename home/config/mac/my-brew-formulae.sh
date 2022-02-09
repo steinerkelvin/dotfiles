61bdd7fbe622d2ad37e1e48bc15faf92cb0a4ddb
Kelvin Santos <kelvinsteinersantos@gmail.com>
2022-02-09 14:50:15 -0300

    formulae

--- home/config/mac/my-brew-formulae.sh
+++ home/config/mac/my-brew-formulae.sh
@@ -16,6 +16,7 @@ brew install watch
 brew install dtach
 brew install jq
 brew install pass
+brew install clac
 
 ## other terminal tools
 brew install tldr
@@ -29,6 +30,7 @@ brew install iperf iperf3
 
 ## dev
 brew install shellcheck
+brew install cloc
 brew install ipython
 brew install yarn
 brew install ngrok
@@ -36,6 +38,13 @@ brew install ngrok
 ## android
 brew install android-platform-tools
 
+
+# gui tools
+
+# brew install --cask rectangle
+brew install --cask hookshot
+brew install --cask hyperkey
+brew install --cask lunar
 
 # apps
 
@@ -49,12 +58,17 @@ brew install --cask telegram-desktop
 brew install --cask whatsapp
 brew install --cask discord
 brew install --cask spotify
+
+brew install --cask speedcrunch
 brew install --cask bitwarden
+brew install --cask todoist
 
 brew install --cask calibre
-brew install --cask logseq
 brew install --cask keybase
+
+brew install --cask logseq
 brew install --cask notion
+brew install --cask typora
 
 brew install --cask gimp
 brew install --cask ledger-live
