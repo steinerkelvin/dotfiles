7b83e271c13af176b8f07f1e1687c233059f5712
Kelvin Steiner Santos <kelvinsteinersantos@gmail.com>
2022-07-31 18:44:40 -0300

    Update system_setup.sh

--- system_setup.sh
+++ system_setup.sh
@@ -1,30 +1,39 @@
 #!/bin/bash
+
+# Update Pacman Index
 pacman -Sy
 
+# Base packages
 pacman -S --needed \
   sudo man vi vim \
   openssh git rsync curl wget \
-  networkmanager avahi nss-mdns \
-  pulseaudio \
-  xdg-user-dirs \
-  gnome-keyring libsecret \
-  brightnessctl acpi \
   arch-install-scripts
 
-systemctl enable NetworkManager
-systemctl start  NetworkManager
+# =====
 
-# graphical
+# Hardware packages
+pacman -S --needed \
+  networkmanager avahi nss-mdns \
+  brightnessctl acpi \
+  pulseaudio
 
+# Gui Packages
 pacman -S --needed \
   sddm \
   arandr autorandr maim \
   i3-gaps i3status i3blocks dmenu rofi dunst libnotify \
   alacritty kitty \
   xclip \
-  nm-connection-editor
+  nm-connection-editor \
+  gnome-keyring libsecret
 
+# =====
+
+# Enable NetworkManager
+systemctl enable NetworkManager
+systemctl start  NetworkManager
+
+# Enable display manager (login thing)
 systemctl enable sddm
 
-echo "Run:"
-echo systemctl start sddm
+echo "Run: systemctl start sddm"
