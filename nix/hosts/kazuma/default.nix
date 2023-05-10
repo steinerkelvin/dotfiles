1fad7631943db41268df41481b2cd8a3a0c12547
Kelvin Steiner <me@steinerkelvin.dev>
2023-05-10 17:41:56 -0300

    add Headscale service

--- nix/hosts/kazuma/default.nix
+++ nix/hosts/kazuma/default.nix
@@ -1,5 +1,10 @@
 { lib, config, pkgs, ... }:
 
+let
+  registeredPorts = {
+    headscale = 49001;
+  };
+in
 {
   imports = [ ../common.nix ./hardware-configuration.nix ];
 
@@ -9,11 +14,30 @@
 
     system.stateVersion = "22.11";
 
+    virtualisation.podman.enable = true;
+
     modules.graphical.enable = true;
 
     modules.services.syncthing.enable = true;
-
     # modules.services.n8n.enable = true;
+
+    networking.firewall = {
+      enable = true;
+      allowedTCPPorts = [ registeredPorts.headscale ];
+    };
+
+    services.headscale = {
+      enable = true;
+      port = registeredPorts.headscale;
+      address = "0.0.0.0";
+      dns = {
+        magicDns = true;
+        baseDomain = "hs.steinerkelvin.dev";
+        domains = [];
+        nameservers =
+          [ "1.1.1.1" "1.0.0.1" "2606:4700:4700::1111" "2606:4700:4700::1001" ];
+      };
+    };
 
     # Bootloader
     boot.loader.efi.canTouchEfiVariables = true;
