9107326aef705c57002c23981ab46299e8284163
Kelvin Steiner <me@steinerkelvin.dev>
2023-07-19 13:04:39 -0300

    extract kazuma arion

--- nix/hosts/kazuma/default.nix
+++ nix/hosts/kazuma/default.nix
@@ -7,6 +7,7 @@ in
   imports = [
     ../common.nix
     ./hardware-configuration.nix
+    ./arion.nix
   ];
 
   config = {
@@ -62,80 +63,6 @@ in
         domains = [];
         nameservers =
           [ "1.1.1.1" "1.0.0.1" "2606:4700:4700::1111" "2606:4700:4700::1001" ];
-      };
-    };
-
-    environment.systemPackages = [
-      pkgs.arion
-      pkgs.docker-client
-      pkgs.docker-compose
-    ];
-
-    virtualisation.arion = {
-      backend = "docker";
-
-      projects = {
-
-        smokeping = {
-          settings.services."smokeping".service = { # Smokeping
-            image = "lscr.io/linuxserver/smokeping:latest";
-            restart = "unless-stopped";
-            ports = [
-              "${toString ports.smokeping}:80"
-            ];
-          };
-        };
-
-        # sticker-bot = {
-        #   settings.services."sticker-bot".service = {
-        #     image = "telegram-inline-stickers-bot-app";
-        #   };
-        # };
-
-        homeassistant = {
-          settings.services.homeassistant.service = { # Home Assistant
-            image = "ghcr.io/home-assistant/home-assistant:stable";
-            restart = "unless-stopped";
-            privileged = true;
-            network_mode = "host";
-            environment = {
-              TZ = "America/Sao_Paulo";
-            };
-            volumes = [
-              "/data/home-assistant/config:/config"
-            ];
-          };
-          settings.services.mqtt.service = { # MQTT
-            image = "eclipse-mosquitto:2.0";
-            restart = "unless-stopped";
-            volumes = [
-              "/data/mosquitto/data:/mosquitto"
-            ];
-            ports = [
-              "${toString ports.mosquitto_1}:1883"
-              "${toString ports.mosquitto_2}:9001"
-            ];
-            command = [ "mosquitto" "-c" "/mosquitto-no-auth.conf" ];
-          };
-          settings.services.zigbee2mqtt.service = { # Zigbee2MQTT
-            image = "koenkk/zigbee2mqtt";
-            restart = "unless-stopped";
-            volumes = [
-              "/data/zigbee2mqtt/data:/app/data"
-              "/run/udev:/run/udev:ro"
-            ];
-            ports = [
-              "${toString ports.zigbee2mqtt}:8080"
-            ];
-            environment = {
-              TZ = "America/Sao_Paulo";
-            };
-            devices = [
-              "/dev/ttyUSB0:/dev/ttyUSB0"
-            ];
-          };
-        };
-
       };
     };
 
