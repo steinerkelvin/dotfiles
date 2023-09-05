a1d77e6a83c7f77edb82a686491ae03a6d327fd2
Kelvin Steiner <me@steinerkelvin.dev>
2023-09-05 12:56:30 -0300

    update discord-krisp-partch for version 0.0.28

--- scripts/discord-krisp-patch.sh
+++ scripts/discord-krisp-patch.sh
@@ -8,8 +8,9 @@ set -e
 rizin_cmd="rizin"
 rz_find_cmd="rz-find"
 
-discord_version="0.0.27"
+discord_version="0.0.28"
 file="${HOME}/.config/discord/${discord_version}/modules/discord_krisp/discord_krisp.node"
 
-addr=$($rz_find_cmd -x '4889dfe8........4889dfe8' "${file}" | head -n1)
-$rizin_cmd -q -w -c "s $addr + 0x12 ; wao nop" "${file}"
+addr=$($rz_find_cmd -x '4881ec00010000' ${file} | head -n1)
+$rizin_cmd -q -w -c "s $addr + 0x30 ; wao nop" "${file}"
+
