f824500708010976e429d26780c3d1238a38597c
Kelvin Steiner <me@steinerkelvin.dev>
2023-11-16 11:54:17 -0300

    Improving `discord-krisp-patch.sh`
    - detects discord version
    - prompts user before patching file
    - informs user which version of Discord is supported / was tested with
    - auto accept flag

--- scripts/discord-krisp-patch.sh
+++ scripts/discord-krisp-patch.sh
@@ -3,14 +3,69 @@
 
 # from: https://github.com/NixOS/nixpkgs/issues/195512#issuecomment-1546794291
 
-set -e
+# The last version of Discord with which this script was tested 
+tested_discord_version="0.0.31"
 
+# External commands that will be used by this script.
+# These lines will be replaced by the nix build.
 rizin_cmd="rizin"
 rz_find_cmd="rz-find"
 
-discord_version="0.0.31"
-file="${HOME}/.config/discord/${discord_version}/modules/discord_krisp/discord_krisp.node"
+set -e
+shopt -s nullglob
 
-addr=$($rz_find_cmd -x '4881ec00010000' ${file} | head -n1)
-$rizin_cmd -q -w -c "s $addr + 0x30 ; wao nop" "${file}"
+auto_accept=""
+for arg in "${@}"; do
+    case "${arg}" in
+        -y)
+            auto_accept="true"
+            ;;
+        *)
+            ;;
+    esac
+done
 
+discord_folder="${HOME}/.config/discord"
+
+# This will be expanded to an empty list if no folder is found
+# as `nullglob` is set
+discord_version_folders=("${discord_folder}"/0.*) 
+
+if [ "${#discord_version_folders[@]}" -eq 0 ]; then
+    echo "ERROR: A folder with the format '${discord_folder}/x.y.z' was not found."
+    exit 1
+fi
+
+# According to People on the Internet™, bash wildcard expansion is lexicographically
+# sorted so I'll just get the last item, that will be the highest
+discord_version_folder="${discord_version_folders[-1]}"
+
+file="${discord_version_folder}/modules/discord_krisp/discord_krisp.node"
+
+if [ ! -f "${file}" ]; then
+    echo "ERROR: '${file}' not found."
+    exit 1
+fi
+
+echo "The last version of discord this script was tested with was ${tested_discord_version}"
+echo "We will patch the file ${file}"
+
+if [ "${auto_accept}" = true ]; then
+    choice="y"
+else
+    echo
+    read -r -p "Proceed (y/[n])? " choice
+    echo
+fi
+
+if [[ ${choice} =~ ^[Yy]$ ]]; then
+    echo
+    echo "Patching..."
+
+    addr=$($rz_find_cmd -x '4881ec00010000' "${file}" | head -n1)
+    $rizin_cmd -q -w -c "s $addr + 0x30 ; wao nop" "${file}"
+
+    echo
+    echo "Done."
+    echo "If you received warnings, you should probably just ignore them."
+fi
