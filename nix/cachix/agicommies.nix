af9cf5d372482d6081817fa439ef7165ea46efc1
Kelvin Steiner <me@steinerkelvin.dev>
2024-05-23 11:42:15 -0300

    disable cachix cache

--- nix/cachix/agicommies.nix
+++ nix/cachix/agicommies.nix
@@ -1,12 +1,12 @@
 {
   nix = {
     settings = {
-      substituters = [
-        "https://agicommies.cachix.org"
-      ];
-      trusted-public-keys = [
-        "agicommies.cachix.org-1:Lol7io35m8Xji9i1Y71GZE2iueEZkeCXU9XtoFK79+A="
-      ];
+      # substituters = [
+      #   "https://agicommies.cachix.org"
+      # ];
+      # trusted-public-keys = [
+      #   "agicommies.cachix.org-1:Lol7io35m8Xji9i1Y71GZE2iueEZkeCXU9XtoFK79+A="
+      # ];
     };
   };
 }
