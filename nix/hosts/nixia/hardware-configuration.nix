a767823d7539452f7a652eb9e6207e7c0e4b2146
Kelvin Steiner <me@steinerkelvin.dev>
2024-06-14 10:10:26 -0300

    nixia: add v4l2loopback

--- nix/hosts/nixia/hardware-configuration.nix
+++ nix/hosts/nixia/hardware-configuration.nix
@@ -9,7 +9,10 @@
   boot.initrd.availableKernelModules =
     [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" ];
+  boot.kernelModules = [
+    "kvm-amd"
+    "v4l2loopback"
+  ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" = {
