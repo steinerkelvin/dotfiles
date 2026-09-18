8ab7ed2c4a7a8ca67478549edde90fee3ce12457
Kelvin Steiner <me@steinerkelvin.dev>
2026-09-18 12:39:00 -0300

    Expose Bash aliases for custom shell startup

--- modules/features/bash.nix
+++ modules/features/bash.nix
@@ -3,7 +3,14 @@
 # (starship.nix), same tool integrations (atuin.nix, zoxide.nix, fzf.nix).
 # Does not change the login shell -- see users/kelvin/account.nix.
 _: {
-  flake.homeModules.bash = _: {
+  flake.homeModules.bash = { config, lib, ... }: {
+    home.file.".bash_aliases".text = lib.concatStringsSep "\n"
+      (
+        lib.mapAttrsToList
+          (name: value: "alias -- ${lib.escapeShellArg name}=${lib.escapeShellArg value}")
+          config.programs.bash.shellAliases
+      ) + "\n";
+
     programs.bash = {
       enable = true;
       initExtra = builtins.readFile ./shell-common.sh;
